f0c90c384c3aa1685d4c407095683e96702ae2eb
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-10-19 15:27:46 +0330

    setup db in settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'itest',
+        'HOST':'127.0.0.1',
+        'USER':'root',
+        'PASSWORD':'h@seyn4651'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
