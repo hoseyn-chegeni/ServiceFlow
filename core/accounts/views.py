d4d1110799878845b1f933502c7ec141b4f0fa90
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-10-20 12:27:32 +0330

    add login view

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+
+
+class UserLogin(LoginView):
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse('index:home')
+    
--- core/index/urls.py
+++ core/index/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import IndexView, UserView
-
+from django.contrib.auth.decorators import login_required
 
 app_name = 'index'
 urlpatterns = [
-    path('',IndexView.as_view(), name='home'),
-    path('users/',UserView.as_view(), name='users'),
+    path('',login_required(IndexView.as_view()), name='home'),
+    path('users/',login_required(UserView.as_view()), name='users'),
 ]
