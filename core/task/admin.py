4a492ed346e8fbb676c8335ff033647369162c88
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-10-20 11:58:49 +0330

    remove task app

--- core/task/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TASK, TaskStatus, TaskType
-
-# Register your models here.
-admin.site.register(TASK)
-admin.site.register(TaskType)
-admin.site.register(TaskStatus)
