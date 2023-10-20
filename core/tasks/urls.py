17f2109286a4c5d9edbd44e6b831f798abd5e141
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-10-20 13:55:58 +0330

    add mytask list

--- core/tasks/urls.py
+++ core/tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskView
+from .views import TaskView, MyTaskView
 
 app_name = 'tasks'
 
 urlpatterns = [
     path('',TaskView.as_view(), name='task_list'),
+    path('mytask/',MyTaskView.as_view(), name='myTask_list'),
 ]
--- core/tasks/views.py
+++ core/tasks/views.py
@@ -1,5 +1,7 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django_filters.views import FilterView
+from django.views.generic import ListView
 from .filters import TaskFilter
 from .models import Task
 
@@ -11,3 +13,12 @@ class TaskView(FilterView):
     paginate_by = 2
     context_object_name = 'task'
     template_name = 'tasks/tasks.html'
+
+
+class MyTaskView(ListView):
+    template_name = 'tasks/myTask.html'
+    model = Task
+    context_object_name = 'tasks'
+    def get_queryset(self, **kwargs):
+       qs = super().get_queryset(**kwargs)
+       return qs.filter(assign_to_id=self.request.user.id)
