ce55a356eea939a63eefb7cdf5d0121b12a77d3e
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-10-25 11:41:17 +0330

    change assign_to field relation to manaytomanyfield

--- core/reminders/models.py
+++ core/reminders/models.py
@@ -3,7 +3,7 @@ from accounts.models import User
 # Create your models here.
 def generate_pk():
     number = (Reminder.objects.all().count())+1
-    return f'NOTE-{number}'
+    return f'REMINDER-{number}'
 
 class Reminder (models.Model):
     reminder= models.CharField(default=generate_pk,max_length=255, unique= True, editable=False, verbose_name='Reminder ID')
@@ -12,7 +12,7 @@ class Reminder (models.Model):
     date = models.DateField()
     time = models.TimeField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
-    assign_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Assign_to')
+    assign_to = models.ManyToManyField(User)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add= True)
     updated_at = models.DateTimeField(auto_now= True)
