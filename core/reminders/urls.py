c020fb9e3f6978df1550bfb56a00e949842b946d
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-10-25 12:20:18 +0330

    add assigned reminder page

--- core/reminders/urls.py
+++ core/reminders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import MyCreatedReminder
+from . views import MyCreatedReminder,AssignedReminder
 
 app_name = 'reminders'
 
 urlpatterns = [
-    path('my_created_reminder/', MyCreatedReminder.as_view(), name='my_created_reminders')
+    path('my_created_reminder/', MyCreatedReminder.as_view(), name='my_created_reminders'),
+    path('assigned_reminder/', AssignedReminder.as_view(), name='assigned_reminder'),
 ]
--- core/reminders/views.py
+++ core/reminders/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Reminder
@@ -10,3 +12,12 @@ class MyCreatedReminder(ListView):
     def get_queryset(self, **kwargs):
        qs = super().get_queryset(**kwargs)
        return qs.filter(created_by_id=self.request.user.id)
+    
+
+class AssignedReminder(ListView):
+    template_name = 'reminder/assigned_reminder.html'
+    model = Reminder
+    context_object_name = 'reminder'
+    def get_queryset(self):
+        user = self.request.user
+        return Reminder.objects.filter(assign_to = user)
