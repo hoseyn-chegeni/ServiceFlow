377dca9d06caca51fe85661fce697930ea41ec11
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-10-25 23:43:31 +0330

    add mail for reminsers

--- core/reminders/apps.py
+++ core/reminders/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class RemindersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reminders'
+    def ready(self):
+        import reminders.signals
---
+++ core/reminders/signals.py
@@ -0,0 +1,18 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import Reminder
+
+@receiver(post_save, sender = Reminder)
+def send_mail_assigned_reminder(sender, instance,created,  **kwargs):
+    if created:
+        subject = f'new reminder '
+        message = f' new reminder assign to you'
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [user for user in instance.assign_to.all()]
+
+        send_mail(subject, message, from_email, recipient_list)
+
+
+post_save.connect(send_mail_assigned_reminder, sender=Reminder)
