b85bb5f416e2ceef5f09f291be84cf528fc0344e
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-10-25 12:39:25 +0330

    add filtering to note app

---
+++ core/notes/filters.py
@@ -0,0 +1,17 @@
+import django_filters
+from django_filters import FilterSet
+from . models import Note
+
+
+class NoteFilter(FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    class Meta:
+        model = Note
+        fields = ['id','title','author', 'is_public', 'author', 'tags',]
+
+
+class PublicNoteFilter(FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    class Meta:
+        model = Note
+        fields = ['id','title','author', 'author', 'tags',]
--- core/notes/views.py
+++ core/notes/views.py
@@ -2,31 +2,37 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Note
+from django_filters.views import FilterView
+from .filters import NoteFilter, PublicNoteFilter
+
 # Create your views here.
 
-class PublicNoteView(ListView):
+class PublicNoteView(FilterView):
     template_name = 'notes/public_notes.html'
     model = Note
     context_object_name = 'notes'
+    filterset_class = PublicNoteFilter
     def get_queryset(self, **kwargs):
        qs = super().get_queryset(**kwargs)
-       return qs.filter(is_public=True)
+       return qs.filter(is_public=True, is_archive = False)
 
     
 
-class MyNotesView(ListView):
+class MyNotesView(FilterView):
     template_name = 'notes/my_notes.html'
     model = Note
+    filterset_class = NoteFilter
     context_object_name = 'notes'
     def get_queryset(self, **kwargs):
        qs = super().get_queryset(**kwargs)
        return qs.filter(author_id=self.request.user.id, is_archive = False)
 
 
-class MyArchiveNotesView(ListView):
+class MyArchiveNotesView(FilterView):
     template_name = 'notes/my_archive_notes.html'
     model = Note
     context_object_name = 'notes'
+    filterset_class = NoteFilter
     def get_queryset(self, **kwargs):
        qs = super().get_queryset(**kwargs)
        return qs.filter(author_id=self.request.user.id, is_archive = True)
