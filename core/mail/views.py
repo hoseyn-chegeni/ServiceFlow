39f262083bf98ee07b439fef2d6a9bff5969f79a
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-04 12:26:20 +0330

    edit image field inside of user models nad add context data inside of inbox views

--- core/mail/views.py
+++ core/mail/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import Mail
 
 # Create your views here.
 class InboxView(TemplateView):
     template_name = 'mail/inbox.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['inbox'] = Mail.objects.filter(recipient = self.request.user)
+        return context
