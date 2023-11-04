49523619d53592eecae966eb19af1104cdb23561
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-04 15:38:48 +0330

    add signals for sending mail to smtp server

---
+++ core/mail/signals.py
@@ -0,0 +1,16 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import Mail
+
+
+@receiver(post_save, sender=Mail)
+def send_to_smtp(sender, instance, created, **kwargs):
+    if created:
+        subject = instance.subject
+        message = instance.body
+        from_email = instance.sender
+        recipient_list = [instance.recipient,]
+
+        send_mail(subject, message, from_email, recipient_list)
