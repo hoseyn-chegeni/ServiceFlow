a05d57bb3396b387e24a85cec90d3aaeeb644f6c
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-05 11:57:50 +0330

    still work on send bulk mail

--- core/reminders/signals.py
+++ core/reminders/signals.py
@@ -3,17 +3,16 @@ from django.db.models.signals import post_save
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import Reminder
+from mail.models import Mail
 
 
 @receiver(post_save, sender=Reminder)
 def send_mail_assigned_reminder(sender, instance, created, **kwargs):
-    if created:
-        subject = f"new reminder "
-        message = f" new reminder assign to you"
-        from_email = settings.EMAIL_HOST_USER
-        recipient_list = [user for user in instance.assign_to.all()]
+        for i in instance.assign_to.all():
+            Mail.objects.create(
+                sender = instance.created_by,
+                recipient = instance.created_by,
+                body = instance.title,
+                subject = instance.title,
+            )
 
-        send_mail(subject, message, from_email, recipient_list)
-
-
-post_save.connect(send_mail_assigned_reminder, sender=Reminder)
