cb262a6d681e51615c96d4e0b95a277ff2fcc9bb
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-08 12:58:10 +0330

    add send mail signals for meetings organizer and attendees

---
+++ core/meetings/signals.py
@@ -0,0 +1,26 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import Meetings
+
+
+@receiver(post_save, sender=Meetings)
+def send_mail_for_attendees(sender, instance, created, **kwargs):
+    if created:
+        subject = f"{instance.organizer} has book a new meeting at {instance.date_time} for you"
+        message = f"{instance.title}, {instance.description}"
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [i for i in instance.attendees.all()]
+        send_mail(subject, message, from_email, recipient_list)
+
+
+@receiver(post_save, sender=Meetings)
+
+def send_mail_for_organizer(sender, instance, created, **kwargs):
+    if created:
+        subject = f"Dear {instance.organizer} yout {instance.title} meeting has set for {instance.date_time}"
+        message = f"{instance.title}, {instance.description}"
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [instance.organizer, "akbar@gmail.com"]
+        send_mail(subject, message, from_email, recipient_list)
