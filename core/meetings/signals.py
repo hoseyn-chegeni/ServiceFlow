5117f952f1b35fba64e6be114657f3dde26d64ab
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-11 15:45:17 +0330

    fix send bulk email issues

--- core/meetings/signals.py
+++ core/meetings/signals.py
@@ -1,17 +1,18 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import Meetings
+from accounts.models import User
 
 
-@receiver(post_save, sender=Meetings)
-def send_mail_for_attendees(sender, instance, created, **kwargs):
-    if created:
+@receiver(m2m_changed, sender=Meetings.attendees.through)
+def send_mail_for_attendees(sender, instance, action, model, pk_set, **kwargs):
+    if action == 'post_add':
         subject = f"{instance.organizer} has book a new meeting at {instance.date_time} for you"
         message = f"{instance.title}, {instance.description}"
         from_email = settings.EMAIL_HOST_USER
-        recipient_list = [i.email for i in instance.attendees.all()]
+        recipient_list = [User.objects.get(pk=pk).email for pk in pk_set]
         send_mail(subject, message, from_email, recipient_list)
 
 
