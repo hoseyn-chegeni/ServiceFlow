27e7cb984ea6a2fdaa9c98151ac551062c594785
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-11 16:09:39 +0330

    add db event model

--- core/db_events/models.py
+++ core/db_events/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
+
+class DatabaseLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    event_type = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    table_name = models.CharField(max_length=255)
+    record_id = models.PositiveIntegerField()
+    field_name = models.CharField(max_length=255)
+    additional_info = models.TextField(blank=True, null=True)
+    is_success = models.BooleanField(default=True)
+
+
+
+    def __str__(self):
+        return f'{self.event_type} on {self.table_name} at {self.timestamp}'
