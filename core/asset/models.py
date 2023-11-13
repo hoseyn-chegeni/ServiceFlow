d9793981c272d1be3e2c7d9f558a6ac8e0926da5
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-13 15:18:38 +0330

    add asset model and status , type and condition model

--- core/asset/models.py
+++ core/asset/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Asset(models.Model):
+    asset_name = models.CharField(max_length=255)
+    asset_type = models.ForeignKey('AssetType', on_delete= models.SET_NULL, blank = True , null=True)
+    manufacturer = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=50, unique=True)
+    purchase_date = models.DateField()
+    warranty_expiry_date = models.DateField(blank = True, null= True)
+    location = models.CharField(max_length=100)
+    assigned_user = models.ForeignKey('accounts.User', on_delete =models.SET_NULL, blank = True, null =True)
+    status = models.ForeignKey('AsetStatus', on_delete = models.SET_NULL, blank = True, null = True)
+    condition = models.ForeignKey('AssetCondition', on_delete = models.SET_NULL, blank = True, null = True)
+    maintenance_schedule = models.CharField(max_length=100, blank =True, null =True)
+    last_maintenance_date = models.DateField()
+    purchase_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    current_value = models.DecimalField(max_digits=10, decimal_places=2)
+    depreciation_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    assigned_ip_address = models.GenericIPAddressField(null=True, blank=True)
+    software_installed = models.TextField(null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.asset_name
+
+
+
+class AssetType(models.Model):
+    pass
+
+class AsetStatus(models.Model):
+    pass
+class AssetCondition(models.Model):
+    pass
