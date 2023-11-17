bdd404c6c7139be6796d5004f12de83b1368f625
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-17 10:44:46 +0330

    add login required mixin to accounts views

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -5,11 +5,12 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .models import User
 from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 
-class UserView(ListView):
+class UserView(LoginRequiredMixin, ListView):
     model = User
     template_name = "registration/user_list.html"
 
@@ -21,36 +22,36 @@ class UserLogin(LoginView):
         return reverse("index:home")
 
 
-class UserLogout(LogoutView):
+class UserLogout( LogoutView):
     def get_success_url(self):
         return reverse("accounts:login")
 
 
-class CreateUser(CreateView):
+class CreateUser(LoginRequiredMixin, CreateView):
     model = User
     form_class = CustomUserCreationForm
     template_name = "registration/signup.html"
     success_url = reverse_lazy("index:home")
 
 
-class UserUpdate(UpdateView):
+class UserUpdate(LoginRequiredMixin, UpdateView):
     model = User
     fields = ("first_name", "last_name", "image")
     template_name = "registration/update.html"
     success_url = reverse_lazy("accounts:users")
 
 
-class UserDetail(DetailView):
+class UserDetail(LoginRequiredMixin, DetailView):
     model = User
     template_name = "registration/detail.html"
 
 
-class UserDelete(DeleteView):
+class UserDelete(LoginRequiredMixin, DeleteView):
     model = User
     template_name = "registration/delete.html"
     success_url = reverse_lazy("accounts:users")
 
 
-class UserPasswordChangeView(PasswordChangeView):
+class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     template_name = "registration/change_password.html"
     success_url = reverse_lazy("index:home")
