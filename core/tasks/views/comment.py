b99b2108b551526bda0bd402a5d5bcdea184d411
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-19 10:14:33 +0330

    Track comment Task and store it in task log table

--- core/tasks/views/comment.py
+++ core/tasks/views/comment.py
@@ -7,6 +7,7 @@ from django.views.generic import (
 from ..models import Task, TaskComment
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from db_events.models import TaskLog
 
 class TaskCommentView(LoginRequiredMixin, CreateView):
     model = TaskComment
@@ -17,6 +18,13 @@ class TaskCommentView(LoginRequiredMixin, CreateView):
         task = get_object_or_404(Task, pk=self.kwargs["pk"])
         form.instance.task = task
         form.instance.user = self.request.user
+
+        TaskLog.objects.create(
+            task=task,
+            user=self.request.user,
+            event_type="Add Comment",
+            additional_info=f"{self.request.user} Added A Comment '{form.instance.comment}' for {task}",
+        )
         return super().form_valid(form)
 
     def get_success_url(self):
