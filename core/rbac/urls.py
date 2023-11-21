ff12883a1d6df19018a7f9bca7e5994b7062363d
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-21 11:52:50 +0330

    assgin role to user from ui done

--- core/rbac/urls.py
+++ core/rbac/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import AssignRoleToUser
 
 app_name = 'rbac'
 
 urlpatterns = [
-
+     path("assign_role_to_user/<int:pk>/", AssignRoleToUser.as_view(), name="assign_role_to_user"),
 ]
--- core/rbac/views.py
+++ core/rbac/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import UserRole
+from accounts.models import User
 # Create your views here.
+
+class AssignRoleToUser(CreateView):
+    model = UserRole
+    fields = ['role',]
+    template_name = 'rbac/assign_role_to_user.html'
+    success_url = reverse_lazy('accounts:users')
+    def form_valid(self, form):
+        user = get_object_or_404(User, pk = self.kwargs["pk"])
+        form.instance.user = user
+        return super().form_valid(form)
+    
