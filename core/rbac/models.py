ca79f7a991967a8ece810a47a738c2808b5ddef2
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-21 11:12:03 +0330

    add userRole model and role model

--- core/rbac/models.py
+++ core/rbac/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import Permission
+
 
 # Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length = 255, )
+    permissions = models.ManyToManyField(Permission)
+    created_by = models.ForeignKey('accounts.User', on_delete = models.SET_NULL, blank = True, null = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    is_active = models.BooleanField(default = True)
+
+
+class UserRole(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete = models.CASCADE)
+    role = models.ForeignKey(Role, on_delete = models.CASCADE)
