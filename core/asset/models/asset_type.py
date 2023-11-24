972a9e4f2ace3838ea9550cad9e3acecd64f324f
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-24 11:38:08 +0330

    add str func to asst type model

--- core/asset/models/asset_type.py
+++ core/asset/models/asset_type.py
@@ -9,3 +9,6 @@ class AssetType(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
