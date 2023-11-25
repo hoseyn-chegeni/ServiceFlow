e20f9425b803c078242d600fa5fd49bf1976ea92
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-25 18:59:52 +0330

    add license and license category to asset app

--- core/asset/admin.py
+++ core/asset/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models.asset import Asset
 from .models.asset_type import AssetType
 from .models.asset_status import AssetStatus
+from .models.license import License,LicenseCategory
 
 # Register your models here.
 
 admin.site.register(Asset)
 admin.site.register(AssetType)
 admin.site.register(AssetStatus)
+admin.site.register(License)
+admin.site.register(LicenseCategory)
---
+++ core/asset/models/license.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+class License(models.Model):
+    license_key = models.CharField(max_length=100)
+    software_name = models.CharField(max_length=100)
+    category_name = models.ForeignKey('LicenseCategory', on_delete = models.SET_NULL, blank = True, null = True)
+    product_key = models.CharField(max_length=100)
+    seats = models.IntegerField()
+    company = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
+    licensed_to_name = models.CharField(max_length=100)
+    licensed_to_email = models.EmailField()
+    reassignable = models.BooleanField(default=False)
+    supplier = models.CharField(max_length=100)
+    order_number = models.CharField(max_length=100)
+    purchase_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateField()
+    expiration_date = models.DateField()
+    termination_date = models.DateField(null=True, blank=True)
+    purchase_order_number = models.CharField(max_length=100, null=True, blank=True)
+    depreciation = models.DecimalField(max_digits=10, decimal_places=2)
+    maintained = models.BooleanField(default=True)
+    notes = models.TextField(blank=True)
+    created_by = models.ForeignKey("accounts.User", on_delete=models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.software_name
+    
+class LicenseCategory(models.Model):
+    name = models.CharField(max_length = 255, )
+    description = models.TextField(blank = True, null = True,)
+    created_by = models.ForeignKey("accounts.User", on_delete=models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
