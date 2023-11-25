70ac45a6175df8bde4ed350c5dacdf429ced9f74
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-25 19:08:27 +0330

    add accessory and accessory category models to asset app

--- core/asset/admin.py
+++ core/asset/admin.py
@@ -3,6 +3,7 @@ from .models.asset import Asset
 from .models.asset_type import AssetType
 from .models.asset_status import AssetStatus
 from .models.license import License,LicenseCategory
+from .models.accessory import Accessory, AccessoryCategory
 
 # Register your models here.
 
@@ -11,3 +12,5 @@ admin.site.register(AssetType)
 admin.site.register(AssetStatus)
 admin.site.register(License)
 admin.site.register(LicenseCategory)
+admin.site.register(Accessory)
+admin.site.register(AccessoryCategory)
---
+++ core/asset/models/accessory.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class Accessory(models.Model):
+    image = models.ImageField(
+        upload_to="images",blank=True, null=True
+    )
+    company = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey('AccessoryCategory', on_delete = models.SET_NULL, blank = True, null = True)
+    model_number = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
+    supplier = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    total = models.PositiveIntegerField(default=0)
+    available = models.PositiveIntegerField(default=0)
+    checked_out = models.PositiveIntegerField(default=0)
+    min_quantity = models.PositiveIntegerField(default=0)
+    purchase_date = models.DateField()
+    purchase_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    order_number = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+    in_out = models.CharField(max_length=10)
+    created_by = models.ForeignKey("accounts.User", on_delete=models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.name
+    
+class AccessoryCategory(models.Model):
+    name = models.CharField(max_length = 255, )
+    description = models.TextField(blank = True, null = True,)
+    created_by = models.ForeignKey("accounts.User", on_delete=models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
