ba96470268aab8d214ada6bc897ebff1e512ae66
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-11-26 16:50:06 +0330

    install bootstrap and add base.html file

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'bootstrap5',
     "rest_framework",
     "drf_yasg",
     "djoser",
