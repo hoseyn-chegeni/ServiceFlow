80e9c61ba2aed73c2ceda2cd9be768ad15234626
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-12-04 10:19:55 +0330

    add masseges for manage priority views

--- core/tasks/views/priority.py
+++ core/tasks/views/priority.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, HttpResponseRedirect
 from django.views.generic import (
-    ListView,
     CreateView,
     DetailView,
     UpdateView,
@@ -14,11 +13,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django_filters.views import FilterView
 from ..filters import PriorityFilter
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+
 class PriorityListView(LoginRequiredMixin, FilterView):
     model = TaskPriority
     context_object_name = "priorities"
     template_name = "tasks/priority/list.html"
     filterset_class = PriorityFilter
+    success_message = "New Priority Successfully Added"
+
 
     def get_paginate_by(self, queryset):
         # Get the value for paginate_by dynamically (e.g., from a form input or session)
@@ -34,9 +39,10 @@ class PriorityDetailView(LoginRequiredMixin, DetailView):
     context_object_name = "priority"
 
 
-class PriorityCreateView(LoginRequiredMixin, CreateView):
+class PriorityCreateView(LoginRequiredMixin, SuccessMessageMixin,  CreateView):
     template_name = "tasks/priority/create.html"
     form_class = CreateTaskPriorityForm
+    success_message = "Priority successfully Created."
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
@@ -45,14 +51,23 @@ class PriorityCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse_lazy("tasks:detail_priority", kwargs={"pk": self.object.pk})
 
+    def get_success_message(self, cleaned_data):
+        return self.success_message
 
-class PriorityUpdateView(LoginRequiredMixin, UpdateView):
+
+class PriorityUpdateView(LoginRequiredMixin,SuccessMessageMixin, UpdateView):
     model = TaskPriority
     fields = ("name", "description", "is_active","badge",)
     template_name = "tasks/priority/update.html"
+    success_message = 'Priority Successfully Updated'
+
 
     def get_success_url(self):
         return reverse_lazy("tasks:detail_priority", kwargs={"pk": self.object.pk})
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message
+
 
 
 class PriorityDeleteView(LoginRequiredMixin, DeleteView):
@@ -72,10 +87,12 @@ class PriorityDeleteView(LoginRequiredMixin, DeleteView):
         return HttpResponseRedirect(success_url)
 
 
-class ChangePriorityView(LoginRequiredMixin, UpdateView):
+class ChangePriorityView(LoginRequiredMixin,SuccessMessageMixin, UpdateView):
     template_name = "tasks/priority/change.html"
     model = Task
     fields = ("priority",)
+    success_message = "Task Priority successfully Changed"
+
 
     def form_valid(self, form):
         task = form.save(commit=False)
@@ -91,3 +108,6 @@ class ChangePriorityView(LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy("tasks:detail", kwargs={"pk": self.object.pk})
+    
+    def get_success_message(self, cleaned_data):
+        return self.success_message
