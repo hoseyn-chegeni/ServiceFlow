92fc8d9669864dc8ee03f3c8845a355dd0676b91
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-12-08 11:37:02 +0330

    add pagination to component model and component list template done

--- core/asset/views/component.py
+++ core/asset/views/component.py
@@ -20,6 +20,14 @@ class ComponentListView(PermissionRequiredMixin, LoginRequiredMixin, FilterView)
     context_object_name = "component"
     permission_required = "asset.view_component"
 
+    def get_paginate_by(self, queryset):
+        # Get the value for paginate_by dynamically (e.g., from a form input or session)
+        # Example: Set paginate_by to a user-selected value stored in session
+        user_selected_value = self.request.session.get(
+            "items_per_page", 10
+        )  # Default to 10
+        return user_selected_value
+
 
 class ComponentDetailView(PermissionRequiredMixin, LoginRequiredMixin, DetailView):
     model = Component
