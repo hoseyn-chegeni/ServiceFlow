39346471d22ee552720a08b4d5e81f5c49d83d82
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-12-09 13:58:17 +0330

    change asset type views to BaseViews (#151)

--- core/asset/views/asset_type.py
+++ core/asset/views/asset_type.py
@@ -1,20 +1,18 @@
-from django.views.generic import (
-    ListView,
-    DeleteView,
-    CreateView,
-    UpdateView,
-    DetailView,
-)
+from django.views.generic import DetailView
 from ..models.asset_type import AssetType
 from django.urls import reverse_lazy
 from ..forms import CreateAssetTypeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from base.views import  BaseCreateView,BaseUpdateView,BaseDeleteView,BaseListView
+from ..filters import AssetTypeFilters
 
 
-class AssetTypeListView(LoginRequiredMixin, ListView):
+class AssetTypeListView(BaseListView):
     model = AssetType
     template_name = "asset/type/list.html"
     context_object_name = "asset"
+    filterset_class = AssetTypeFilters
+    permission_required = 'asset.view_assettype'
 
 
 class AssetTypeDetailView(LoginRequiredMixin, DetailView):
@@ -22,31 +20,39 @@ class AssetTypeDetailView(LoginRequiredMixin, DetailView):
     template_name = "asset/type/detail.html"
 
 
-class AssetTypeCreateView(LoginRequiredMixin, CreateView):
+class AssetTypeCreateView(BaseCreateView):
     model = AssetType
     template_name = "asset/type/create.html"
     form_class = CreateAssetTypeForm
+    permission_required = 'asset.add_assettype'
+    success_message = 'Type Successfully Created.'
+
 
     def get_success_url(self):
-        return reverse_lazy("asset:type_list")
-
-    def form_valid(self, form):
-        form.instance.created_by = self.request.user
-        return super().form_valid(form)
+        return reverse_lazy("asset:type_detail", kwargs={"pk": self.object.pk})
 
 
-class AssetTypeUpdateView(LoginRequiredMixin, UpdateView):
+
+
+
+class AssetTypeUpdateView(BaseUpdateView):
     model = AssetType
     template_name = "asset/type/update.html"
+    permission_required = 'asset.change_assettype'
+    success_message = 'Type Successfully Updated.'
     fields = (
         "name",
         "description",
     )
 
-    success_url = reverse_lazy("asset:type_list")
+    def get_success_url(self):
+        return reverse_lazy("asset:type_detail", kwargs={"pk": self.object.pk})
 
 
-class AssetTypeDeleteView(LoginRequiredMixin, DeleteView):
+class AssetTypeDeleteView(BaseDeleteView):
     model = AssetType
     template_name = "asset/type/delete.html"
     success_url = reverse_lazy("asset:type_list")
+    permission_required = 'asset.delete_assettype'
+    message = 'Type Successfully Deleted'
+
