b3d55f577fb18e42238cadd0a4c15ea04e34e64e
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-12-10 13:54:08 +0330

    change consumable views to BaseViews (#151)

--- core/asset/views/consumable.py
+++ core/asset/views/consumable.py
@@ -6,9 +6,8 @@ from django.views.generic import (
 )
 from ..models.consumable import ConsumableCategory, Consumable
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from ..filters import ConsumableFilters
-from base.views import BaseListView, BaseDeleteView
+from base.views import BaseListView, BaseDeleteView, BaseCreateView,BaseDetailView,BaseUpdateView
 
 
 # CONSUMABLE Views Here...
@@ -20,32 +19,31 @@ class ConsumableListView(BaseListView):
     filterset_class = ConsumableFilters
 
 
-class ConsumableDetailView(PermissionRequiredMixin, LoginRequiredMixin, DetailView):
+class ConsumableDetailView(BaseDetailView):
     model = Consumable
     template_name = "asset/consumable/detail.html"
     permission_required = "asset.view_consumable"
 
 
-class ConsumableCreateView(PermissionRequiredMixin, LoginRequiredMixin, CreateView):
+class ConsumableCreateView(BaseCreateView):
     model = Consumable
     template_name = "asset/consumable/create.html"
     fields = "__all__"
     permission_required = "asset.add_consumable"
+    success_message = 'Consumable Successfully Created.'
 
     def get_success_url(self):
         return reverse_lazy("asset:consumable_list")
 
-    def form_valid(self, form):
-        form.instance.created_by = self.request.user
-        return super().form_valid(form)
 
 
-class ConsumableUpdateView(PermissionRequiredMixin, LoginRequiredMixin, UpdateView):
+class ConsumableUpdateView(BaseUpdateView):
     model = Consumable
     template_name = "asset/consumable/update.html"
     fields = "__all__"
     permission_required = "asset.change_consumable"
     success_url = reverse_lazy("asset:consumable_list")
+    success_message = 'Consumable Successfully Created.'
 
 
 class ConsumableDeleteView(BaseDeleteView):
