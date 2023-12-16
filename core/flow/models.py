419085d7aeef7e6f840bb3d3fe3adc8787cbc674
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-12-16 10:53:34 +0330

    write models for workFlow action and state in workFlow app (#157)

--- core/flow/models.py
+++ core/flow/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class WorkFlow(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+    root = models.OneToOneField('State',on_delete = models.CASCADE)
+    diagram = models.ImageField(upload_to="images")
+    created_by = models.ForeignKey("accounts.User", on_delete=models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class State(models.Model):
+    state = models.CharField(max_length = 255, )
+    team  = models.ForeignKey('team.Team', on_delete = models.CASCADE)
+    process_percentage = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    action = models.ForeignKey('Action', on_delete = models.CASCADE)
+    
+class Action(models.Model):
+    title = models.CharField(max_length = 255)
+    next_state = models.ForeignKey('State',on_delete = models.SET_NULL, blank = True, null = True, related_name = 'next')
+    
